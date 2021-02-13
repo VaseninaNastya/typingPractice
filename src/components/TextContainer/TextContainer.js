1ddef8bef2c65346317aa8fb77b215991627770b
VaseninaNastya <dadasiegt@yandex.ru>
2021-02-13 21:46:32 +0300

    fix: fix obserwer

--- src/components/TextContainer/TextContainer.js
+++ src/components/TextContainer/TextContainer.js
@@ -26,8 +26,13 @@ class TextContainer {
     this.lastLetterIndex = this.textDataArr.length - 1;
     console.log("lastLetterIndex", this.lastLetterIndex);
     this.lettersNodes = document.querySelectorAll(".letter");
-    console.log("Array.from(this.lettersNodes)",Array.from(this.lettersNodes)[this.curLetterIndex]);
-    Array.from(this.lettersNodes)[this.curLetterIndex].classList.add("letter__active");
+    console.log(
+      "Array.from(this.lettersNodes)",
+      Array.from(this.lettersNodes)[this.curLetterIndex]
+    );
+    Array.from(this.lettersNodes)[this.curLetterIndex].classList.add(
+      "letter__active"
+    );
   }
   generateLayout() {
     const infoBlock = new InfoBlock();
@@ -40,14 +45,28 @@ class TextContainer {
   }
   addEventListeners() {
     document.querySelector("body").addEventListener("keydown", (e) => {
-      if (document.querySelector(".letter__active").innerHTML == e.key) {
-        if (this.curLetter != this.lastLetterIndex) {
+      if (this.curLetter != this.lastLetterIndex && !e.key.includes("Shift")) {
+        if (document.querySelector(".letter__active").innerHTML == e.key){
           console.log("работает", this.curLetterIndex);
           this.curLetterIndex++;
           console.log("работает   111", this.curLetterIndex);
-          document.querySelector(".letter__active").classList.remove("letter__active")
-          Array.from(this.lettersNodes)[this.curLetterIndex].classList.add("letter__active");
-        } else {console.log("конец");}
+          document
+            .querySelector(".letter__active")
+            .classList.remove("letter__active");
+          if(document.querySelector(".letter__error")){
+            document.querySelector(".letter__error").classList.remove("letter__error")
+          }
+          Array.from(this.lettersNodes)[this.curLetterIndex].classList.add(
+            "letter__active"
+          );
+        } else {
+          Array.from(this.lettersNodes)[this.curLetterIndex].classList.add(
+            "letter__error"
+          );
+        }
+      }
+      if (this.curLetter == this.lastLetterIndex){
+        console.log("конец");
       }
     });
   }
